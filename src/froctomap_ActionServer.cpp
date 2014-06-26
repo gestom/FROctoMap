33f7fed2279df1de47b505dbf6968ec2d03fba66
Santos <santos@santos-P15xEMx.(none)>
2014-06-26 13:33:31 +0100

    everything working except the save action

--- src/froctomap_ActionServer.cpp
+++ src/froctomap_ActionServer.cpp
@@ -3,11 +3,11 @@
 #include <actionlib/server/simple_action_server.h>
 #include "fremen/UpdateGrid.h"
 #include "fremen/SaveGrid.h"
-#include "fremen/RetrieveOctomap.h"
+#include "fremen/RecoverOctomap.h"
 #include "fremen/EstimateOctomap.h"
 
 
-ros::ServiceClient *predict_client_ptr, *update_client_ptr, *save_client_ptr, *estimate_client_ptr, *retrieve_client_ptr;
+ros::ServiceClient *predict_client_ptr, *update_client_ptr, *save_client_ptr, *estimate_client_ptr, *recover_client_ptr;
 
 typedef actionlib::SimpleActionServer<fremen::froctomapAction> Server;
 
@@ -15,22 +15,7 @@ void execute(const fremen::froctomapGoalConstPtr& goal, Server* as)
 {
 
   if(goal->name_action == "predict"){
-    ROS_INFO("Predicting...!");
-   // fremen::GenerateOctomap srv;
-    //srv.request.stamp = goal->stamp;
-  
-   /* if(predict_client_ptr->call(srv)){
-      if(srv.response.result){
-	ROS_INFO("Octomap generated!");
-	as->setSucceeded();
-      }else{
-	ROS_INFO("Failed to generate the octomap!");
-	as->setAborted();
-      }
-    }else{
-      ROS_ERROR("Failed to call service generate_octomap");
-      as->setAborted();
-      }*/
+    //Service replaced by estimate!
       
   }else if(goal->name_action == "update"){
     ROS_INFO("Updating...!");
@@ -60,19 +45,19 @@ void execute(const fremen::froctomapGoalConstPtr& goal, Server* as)
       ROS_ERROR("Failed to call service save_grid");
       as->setAborted();
     }
-  }else if(goal->name_action == "recover_octomap"){
+  }else if(goal->name_action == "recover"){
     ROS_INFO("Recovering...!");
-    fremen::RetrieveOctomap srv;
+    fremen::RecoverOctomap srv;
     srv.request.stamp = goal->stamp;
   
-    if (retrieve_client_ptr->call(srv)){
-      ROS_INFO("Octomap retrieved!");
+    if (recover_client_ptr->call(srv)){
+      ROS_INFO("Octomap recovered!");
       as->setSucceeded();
     }else{
-      ROS_ERROR("Failed to call service RetrieveOctomap");
+      ROS_ERROR("Failed to call service RecoverOctomap");
       as->setAborted();
     }
-  }else if(goal->name_action == "estimate_octomap"){
+  }else if(goal->name_action == "estimate"){
     ROS_INFO("Estimating...!");
     fremen::EstimateOctomap srv;
     srv.request.stamp = goal->stamp;
@@ -100,16 +85,14 @@ int main(int argc,char *argv[])
   ros::NodeHandle n;
   
   //Services:
- // ros::ServiceClient predict_client = n.serviceClient<fremen::GenerateOctomap>("generate_octomap");
- // predict_client_ptr = &predict_client;
   ros::ServiceClient update_client = n.serviceClient<fremen::UpdateGrid>("update_grid");
   update_client_ptr = &update_client;
   ros::ServiceClient save_client = n.serviceClient<fremen::SaveGrid>("save_grid");
   save_client_ptr = &save_client;
   ros::ServiceClient estimate_client = n.serviceClient<fremen::EstimateOctomap>("estimate_octomap");
   estimate_client_ptr = &estimate_client;
-  ros::ServiceClient retrieve_client = n.serviceClient<fremen::RetrieveOctomap>("retrieve_octomap");
-  retrieve_client_ptr = &retrieve_client;
+  ros::ServiceClient recover_client = n.serviceClient<fremen::RecoverOctomap>("recover_octomap");
+  recover_client_ptr = &recover_client;
   
   
   
