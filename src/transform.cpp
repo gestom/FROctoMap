91df5639701de7933c6bf50ed893f6134e3a2f41
gestom <tkrajnik@lincoln.ac.uk>
2014-06-27 11:53:08 +0100

    Point cloud transform

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,123 +1,53 @@
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/PointCloud.h>
-#include <geometry_msgs/Pose.h>
-#include <std_msgs/String.h>
-#include <pcl/filters/voxel_grid.h>
-// PCL specific includes
-#include <sensor_msgs/point_cloud_conversion.h>
-#include <pcl/ros/conversions.h>
+#include "ros/ros.h"
+#include <ros/time.h>
+#include "sensor_msgs/PointCloud2.h"
+#include <tf/transform_listener.h>
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+
 #include <pcl_ros/transforms.h>
+#include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/PCLPointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl_ros/filters/passthrough.h>
 
+#include <string>
 
-float x,y,a;
-float gridResolution = 0.1;
+using namespace std;
+
 ros::Publisher pub;
-int pcCounter = 0;
-pcl::PointCloud<pcl::PointXYZ>::Ptr completeCloud(new pcl::PointCloud<pcl::PointXYZ>);
+tf::TransformListener *listener;
+tf::MessageFilter<sensor_msgs::PointCloud2> *tf_filter;
 
-void pose_cb (const geometry_msgs::PosePtr& pose)
+void callback(const boost::shared_ptr<const sensor_msgs::PointCloud2>& msg)
 {
-	if (pcCounter > 0 && pcCounter < 10){
-		x = pose->position.x;
-		y = pose->position.y;
-		printf("Position updated %f %f\n",x,y);
-	}
+  //ROS_INFO("Called callback");
+  sensor_msgs::PointCloud2 pc_out;
+  listener->waitForTransform(msg->header.frame_id.c_str(), "/", ros::Time(), ros::Duration(3.0));
+  pcl_ros::transformPointCloud("/head_xtion_rgb_optical_frame", *msg, pc_out, *listener);
+  pub.publish(pc_out);
 }
 
-void name_cb (const std_msgs::StringPtr& input)
+int main(int argc, char **argv)
 {
-	//if (input->data == "Aisle2")
-	if (input->data == "Ramp2")
-	{
-		printf("Starting to listen\n");
-		pcCounter = 0;
-	}
-}
+  ros::init(argc, argv, "transform_pc2");
 
+  ros::NodeHandle n;
 
-void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& inputCloud)
-{
-	tf::TransformListener tl;
-	sensor_msgs::PointCloud2 cloud2;
-	tf::Transform tf;
-	tl.waitForTransform("/map", "/head_xtion_depth_optical_frame",inputCloud->header.stamp, ros::Duration(20.0));
-	tl.lookupTransform("/map","/head_xtion_depth_optical_frame", inputCloud->header.stamp, tf);
-	pcl_ros::transformPointCloud("/head_xtion_depth_optical_frame",tf,inputCloud,cloud2);
-/*	pcl::PCLPointCloud cloud;
-	sensor_msgs::convertPointCloud2ToPointCloud(inputCloud,cloud);
-	TfListener.transformPointCloud("/head_xtion_depth_optical_frame", ,);
-	sensor_msgs::convertPointCloudToPointCloud2(pcWorldFrame,pc2WorldFrame) 
+  listener = new tf::TransformListener();
 
+  string pc_sub_topic;
+  string pc_pub_topic;
 
-	pcl::PCLPointCloud2 cloud2;
-	tf::TransformListener listener;
-	listener.lookupTransform("/head_xtion_depth_optical_frame", "/map",  ros::Time(0), transform);
-	transformPointCloud("/head_xtion_depth_optical_frame","/map",inputCloud,cloud2);*/
-}
+  ros::NodeHandle private_node_handle_("~");
+  private_node_handle_.param("pc2_input", pc_sub_topic, string("/local_metric_map/merged_point_cloud"));
+  message_filters::Subscriber<sensor_msgs::PointCloud2> sub;
+  sub.subscribe(private_node_handle_, pc_sub_topic.c_str(), 10);
+  tf_filter = new tf::MessageFilter<sensor_msgs::PointCloud2>(sub, *listener, "/head_xtion_rgb_optical_frame", 10);
+  tf_filter->registerCallback(boost::bind(&callback, _1));
+  private_node_handle_.param("pc2_output", pc_pub_topic, string("/froctomap/localpoints"));
+  pub = n.advertise<sensor_msgs::PointCloud2>(pc_pub_topic.c_str(), 10);
 
+  ros::spin();
 
-
-void cloud_cb_fuck (const sensor_msgs::PointCloud2ConstPtr& inputCloud)
-{
-	if (pcCounter++ < 67){
-		pcl::PCLPointCloud2 cloud2;
-		pcl::PCLPointCloud2 output2;
-		Eigen::Matrix4f scale;
-		scale << 1,0,0,0,
-		      0,1,0,0,
-		      0,0,1,0,
-		      0,0,0,1;
-		pcl::PointCloud<pcl::PointXYZ> cloud1;
-		pcl::PointCloud<pcl::PointXYZ> output1;
-		pcl_conversions::toPCL(*inputCloud, cloud2);
-		pcl::fromPCLPointCloud2(cloud2, cloud1);
-		*completeCloud = cloud1;
-		std::cout << completeCloud->size() << std::endl; 
-			
-		pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
-		/*voxel_grid.setInputCloud (completeCloud);
-		voxel_grid.setLeafSize (0.1, 0.1, 0.1);
-		voxel_grid.filter (cloud1);
-		pcl::transformPointCloud (cloud1, output1,scale);*/
-
-/*		pcl::toPCLPointCloud2(*completeCloud, output2);
-		output2.header.frame_id = "/head_xtion_rgb_optical_frame";
-		pub.publish (output2);*/
-		if (pcCounter == 67){
-			pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
-			voxel_grid.setInputCloud (completeCloud);
-			voxel_grid.setLeafSize (0.05, 0.05, 0.05);
-			voxel_grid.filter (cloud1);
-			pcl::transformPointCloud (cloud1, output1,scale);
-			pcl::toPCLPointCloud2(output1, output2);
-			output2.header.frame_id = "/map";
-			pub.publish (output2);
-		}
-	}
-	printf("%i\n",pcCounter);
-}
-
-int main (int argc, char** argv)
-{
-	x=y=a=0;
-	// Initialize ROS
-	ros::init (argc, argv, "my_pcl_tutorial");
-	ros::NodeHandle nh;
-
-	// Create a ROS subscriber for the input point cloud
-	ros::Subscriber subcloud = nh.subscribe ("/local_metric_map/merged_point_cloud", 1, cloud_cb);
-	ros::Subscriber subname = nh.subscribe ("/ptu_sweep/current_node", 1, name_cb);
-	ros::Subscriber subpose = nh.subscribe ("/robot_pose", 1, pose_cb);
-
-	// Create a ROS publisher for the output point cloud
-	pub = nh.advertise<sensor_msgs::PointCloud2> ("/froctomap/localpoints", 1);
-
-	// Spin
-	ros::spin ();
+  return 0;
 }
